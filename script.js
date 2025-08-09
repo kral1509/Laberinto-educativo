a31d9fa0ee643a3caa4003f28364e6f8714e8c2c
Nivi01pro <nivaldocantosn@gmail.com>
2025-08-08 19:57:38 -0500

    cargar sprites y funciones de los botones

--- script.js
+++ script.js
@@ -60,3 +60,81 @@ document.addEventListener("keydown", (e) => {  // Tecla ESC para regresar al men
 }
 
 });
+
+// Precarga inicial del personaje por defecto
+preCargarSprites(personajeSeleccionado);
+
+// Función para precargar sprites
+function preCargarSprites(nombreArchivo) {
+  const baseNombre = nombreArchivo.split(".")[0].toLowerCase();
+  const max = spritesPorDireccion[nombreArchivo];
+  const direcciones = ["up", "down", "left", "right"];
+
+  imagenesCargadas[baseNombre] = {};
+
+  direcciones.forEach(dir => {
+    imagenesCargadas[baseNombre][dir] = [];
+    for (let i = 1; i <= max; i++) {
+      const img = new Image();
+      img.src = Recursos/${baseNombre}${i}${dir}.png;
+      imagenesCargadas[baseNombre][dir].push(img);
+    }
+  });
+
+  const idle = new Image();
+  idle.src = Recursos/${baseNombre}Idle.png;
+  imagenesCargadas[baseNombre]["idle"] = idle;
+}
+
+// Selector del personaje desde el menú
+document.getElementById("seleccion-personaje").addEventListener("change", function () {
+  personajeSeleccionado = this.value;
+  document.getElementById("imagen-personaje").src = "Recursos/" + personajeSeleccionado;
+  preCargarSprites(personajeSeleccionado);
+});
+
+function mostrarJuego() {
+  document.getElementById("menu-principal").classList.add("oculto");
+  document.getElementById("pantalla-niveles").classList.add("oculto");
+  document.getElementById("pantalla-juego").classList.remove("oculto");
+  iniciarNivel(0);
+}
+
+function volverAlMenu() {
+  document.getElementById("pantalla-juego").classList.add("oculto");
+  document.getElementById("pantalla-niveles").classList.add("oculto");
+  document.getElementById("menu-principal").classList.remove("oculto");
+}
+
+function salir() {
+  alert("Gracias por jugar :)");
+}
+
+function irAlMenu() {
+  document.getElementById("pantalla-inicio").classList.add("oculto");
+  document.getElementById("menu-principal").classList.remove("oculto");
+}
+
+function mostrarNiveles() {
+  document.getElementById("menu-principal").classList.add("oculto");
+  document.getElementById("pantalla-niveles").classList.remove("oculto");
+}
+
+function iniciarNivel(nivel) {
+  nivelActual = nivel;
+  mapa = JSON.parse(JSON.stringify(mapasPorNivel[nivelActual]));
+
+  let encontrado = false;
+  for (let y = 0; y < mapa.length; y++) {
+    for (let x = 0; x < mapa[0].length; x++) {
+      if (mapa[y][x] === 4) {
+        if (!encontrado) {
+          posX = x * tileSize + tileSize / 2;
+          posY = y * tileSize + tileSize / 2;
+          destinoX = posX;
+          destinoY = posY;
+          encontrado = true;
+        }
+        mapa[y][x] = 0;
+      }
+    }
