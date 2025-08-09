3f55823c54dab0021c440e6fd41cf23ab24c7f01
kral1509 <jargudov1@est.ups.edu.ec>
2025-08-08 19:41:19 -0500

    Diseño del juego, y variables definidas

---
+++ script.js
@@ -0,0 +1,35 @@
+let nivelActual = 0;
+let mapa = [];
+const tileSize = 48;
+
+const spritesPorDireccion = {
+  "pikachu.png": 3,
+  "luigi.png": 8,
+  "cuphead.png": 10,
+};
+
+let personajeSeleccionado = "pikachu.png";
+let direccion = "down";
+let frame = 1;
+let posX = 0;
+let posY = 0;
+let destinoX = 0;
+let destinoY = 0;
+let moviendo = false;
+let distanciaDesdeInicio = 0;
+const imagenesCargadas = {};
+const texturasMapa = {
+  camino: new Image(),
+  muro: new Image(),
+};
+
+texturasMapa.camino.src = "Recursos/mapa1camino.png";
+texturasMapa.muro.src = "Recursos/mapa1muro.png";
+
+let ultimoCambioSprite = Date.now();
+const tiempoEntreSprites = 60;
+const velocidad = 6;
+let loopID = null;
+let tiempoRestante = 30;
+let intervaloTiempo = null;
+let teclasPresionadas = {};
